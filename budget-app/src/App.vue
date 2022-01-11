569619c0b54a43230c9d4aa33541bb1b86df804c
Dima <skaizy44@gmail.com>
2022-01-11 21:53:09 +0200

    s
    s

---
+++ budget-app/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="app">
+    <BudgetList />
+  </div>
+</template>
+
+<script>
+import BudjetList from '@/components/BudjetList.vue';
+
+export default {
+  name: 'App',
+  comment: {
+    BudjetList,
+  },
+  data: () => ({
+    1: {
+      type: 'INCOME',
+      value: 100,
+      comment: 'Some comment',
+      id: 1,
+    },
+     2: {
+      type: 'OUTCOME',
+      value: -50,
+      comment: 'Some outcome comment',
+      id: 2,
+    },
+  }),
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ budget-app/src/plugins/elemenst/index.js
@@ -0,0 +1,25 @@
+import Vue from 'vue';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+import 'element-ui/packages/theme-chalk/lib/index.css';
+import {
+    Button,
+    Card,
+    FormItem,
+    Imput,
+    Select,
+    Option
+} from 'element-ui';
+
+const elements = [
+    Button,
+    Card,
+    FormItem,
+    Imput,
+    Select,
+    Option
+];
+
+locale.use(lang);
+
+elements.forEach( El => Vue.use(El, { locale }));
